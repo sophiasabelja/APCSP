18b2d8a4b7dd95ace9f033f69a234375c6f995d5
sophiasabelja <54071882+sophiasabelja@users.noreply.github.com>
2020-01-07 12:58:26 -0500

    Update NYT_Breaking_News_API.py

--- NYT_Breaking_News_API.py
+++ NYT_Breaking_News_API.py
@@ -1,2 +1,6 @@
 # https://developer.nytimes.com/docs/top-stories-product/1/overview
+#*****************
+#API from New York Times Breaking News Politics JSON Library
+# https://api.nytimes.com/svc/topstories/v2
+#***********
 /{politics}.json
